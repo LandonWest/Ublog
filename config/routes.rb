07febfb248d8c07740acbaad012b9aad1fee1b1c
LandonWest <landonwest5@gmail.com>
2014-06-19 17:37:50 -0600

    Add Post scaffold, validate users, show posts and so much more...

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :posts
+
   root 'static#index'
   devise_for :users
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140619195551) do
+ActiveRecord::Schema.define(version: 20140619220221) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "message"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
